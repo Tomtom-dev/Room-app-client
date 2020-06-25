4a3d0a2571eef22c898f80c2e2d741af4f772dc2
Tomtom-dev <thomasheddadji0@gmail.com>
2020-06-25 23:16:57 +0200

    ad feature to select by country

--- src/pages/Rooms/index.js
+++ src/pages/Rooms/index.js
@@ -1,26 +1,47 @@
-import React, {useState,useEffect} from 'react'
-import {useDispatch, useSelector} from 'react-redux'
-import {fetchRooms} from '../../store/roomPage/action'
-import {getRoomPage} from '../../store/roomPage/selector'
-import RoomPageCard from '../../component/RoomPageCard'
-
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchRooms } from "../../store/roomPage/action";
+import { getRoomPage } from "../../store/roomPage/selector";
+import RoomPageCard from "../../component/RoomPageCard";
+import Select from "react-select";
 
 export default function Rooms() {
+  const roomPages = useSelector(getRoomPage);
+  const dispatch = useDispatch();
 
-    const roomPages= useSelector(getRoomPage)
-    const dispatch = useDispatch()
+  const [address, setAddress] = useState([]);
 
-    useEffect(()=>{
-        dispatch(fetchRooms())
-    },[])
+  console.log("theroompage", roomPages);
 
-    const roomsToRender = () =>
-        roomPages.map(room => <RoomPageCard key={room.id}{...room}/>)
-    
-    return (
-        <div>
-            <h1>Rooms</h1>
-            {roomsToRender()}
-        </div>
-    )
+  useEffect(() => {
+    dispatch(fetchRooms());
+  }, []);
+
+  //get array of location 
+
+  const arrayOfLocation=roomPages.map(room=>{
+    return{
+        key:room.id,
+        value:room.location,
+        label:room.location
+    }
+  })
+
+  console.log(arrayOfLocation);
+  
+
+  const roomsToRender = () =>
+    roomPages.map((room) => <RoomPageCard key={room.id} {...room} />);
+
+  return (
+    <div>
+      <h1>Rooms</h1>
+        <form style={{width:"15em", marginLeft:"24em", marginBottom:"2em", margin:'auto'}}>
+             <Select options={arrayOfLocation}/>
+        </form>
+      
+
+      {roomsToRender()}
+    </div>
+  );
 }
