a2f3ba8df8ec2d5501da595d5c74b262fdac861c
Tomtom-dev <thomasheddadji0@gmail.com>
2020-06-25 14:44:00 +0200

    add styling for roomPage and fix the google map API

--- src/component/RoomPageCardDetail.js
+++ src/component/RoomPageCardDetail.js
@@ -1,30 +1,51 @@
 import React,{useState} from 'react'
 import { useSelector } from "react-redux";
-import {GoogleMap, useLoadScript, Marker, InfoWindow} from "@react-google-maps/api"
+import Geocode from "react-geocode"
+import {GoogleMap, useLoadScript, Marker, InfoWindow, Autocomplete} from "@react-google-maps/api"
 import {selectRoomDetails} from '../store/roomPageDetail/selector'
 import mapStyles from "./mapStyles"
 import {formatRelative} from 'date-fns'
+import './RoomPageCardDetail.css'
 
 
 export default function RoomPageCardDetail(props) {
 
     const{title,image,description,location,id}= props
 
-    const adressForRoom = useSelector(selectRoomDetails).location
-
-    console.log('please give me the room adress great sir', adressForRoom);
-
-    const [markers, setMarkers] = useState([])
+    const [lng, setLng] = useState([])
+    const [lat, setLat] = useState([])
 
     const REACT_APP_GOOGLE_MAPS_API_KEY="AIzaSyDaBw7bLrb_TtZx2Ei2os4dSvQJ85nRW9c"
+
+    // get the adress
+    const adressForRoom = useSelector(selectRoomDetails).location
+
+    //transform the adress to latitude and longitude with geocoding
+
+    Geocode.setApiKey(REACT_APP_GOOGLE_MAPS_API_KEY);
+    Geocode.setLanguage("en")
+    Geocode.fromAddress(adressForRoom).then(
+        response => {
+          const { lat, lng } = response.results[0].geometry.location;
+          console.log(lat, lng);
+          setLng(lng)
+          setLat(lat)
+
+        },
+        error => {
+          console.error(error);
+        }
+      );
+
     const libraries= ["places"];
     const mapContainerStyle={
-        width:"100vw",
-        height:"100vh"
+        width:"750px",
+        height:"350px",
+        margin:"auto"
     }
     const center = {
-        lat:52.370216,
-        lng:4.895168
+        lat:lat,
+        lng:lng
     }
 
     const options={
@@ -46,24 +67,31 @@ export default function RoomPageCardDetail(props) {
 
     return (
         <div>
-            <section>  
-                <h2>Offer Details</h2>
-                <img src={image} alt={"the pic"}/>
-                <h2>{title}</h2>
+            <h2>Offer Details</h2>
+            <section className="formRoomDetail">  
+                
+                <img src={image} alt={"the pic"} id="img-presentation"/>
+                
                 <div>
+                    <h3>{title}</h3>
                     <p>{location}</p>
                     <p>{description}</p>
                 </div> 
                 <div>
-                    <h3>Room </h3>
                     <GoogleMap 
                     mapContainerStyle={mapContainerStyle} 
-                    zoom={8} 
+                    zoom={12} 
                     center={center}
                     options={options}
-                    onClick={(event)=>{setMarkers()}
-                    }
-                    ></GoogleMap>
+                    >
+
+                        <Marker 
+                        position={
+                            {lat:lat,
+                            lng:lng}
+                        }/>
+
+                    </GoogleMap>
                 </div>
             </section>
         </div>
