6b03230755d83be0eba5a0fb81a81a9302defa1b
Tomtom-dev <thomasheddadji0@gmail.com>
2020-07-06 08:41:26 +0200

    add slider sort with filters

--- src/pages/Rooms/index.js
+++ src/pages/Rooms/index.js
@@ -1,77 +1,56 @@
-import React, {  useEffect } from "react";
+import React, {  useEffect,useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchRooms } from "../../store/roomPage/action";
 import { getRoomPage } from "../../store/roomPage/selector";
 import RoomPageCard from "../../component/RoomPageCard";
-import Select from "react-select";
+
 
 export default function Rooms() {
   const roomPages = useSelector(getRoomPage);
   const dispatch = useDispatch();
-
-  const [place, setPlace] = React.useState(roomPages[0])
-  // const [inputValue, setInputValue] = useState([])
-
+  const [place, setPlace] = useState("value")
 
   useEffect(() => {
     dispatch(fetchRooms());
   }, [dispatch]);
 
-  //get array of location 
+  // Filter with the slider
 
-  const arrayOfLocation=roomPages.map(room=>{
-    return{
-        key:room.id,
-        value:room.location,
-        label:room.location
+  let filteredRooms = roomPages.filter(room=>{
+
+
+    if (room.location === "choose a location"){
+      return true
+    }else {
+      const matchRoom = room.location === place
+      if(matchRoom){
+          return true
+      }else{
+          return false
+      }
     }
   })
 
-  //get the current value of the slider
-
-  // const currentLocation = (event) =>{
-  //     const locationChoose= (event.target.value)
-  //     setPlace(locationChoose)
-  // }
-
-  // Filter with the sliders
-
-  // let filteredRooms = roomPages.filter(room=>{
-  //     const matchRoom = room.location === place
-  //     if(matchRoom){
-  //         return true
-  //     }else{
-  //         return false
-  //     }
-  // })
-  
-
-  // console.log("place",place);
-
-  // console.log("roomPage content", roomPages);
-  
-  // const currentValueSelect =(event) =>{
-  //   const value= event.target.value
-  //   setInputValue
-  // }
-
+  console.log("filter",filteredRooms);
 
   const roomsToRender = () =>
-    roomPages.map((room) => <RoomPageCard key={room.id} {...room} />);
+    filteredRooms.map((room) => <RoomPageCard key={room.id} {...room} />);
 
   return (
     <div>
       <h1>Rooms</h1>
-        <form style={{width:"15em", marginLeft:"24em", marginBottom:"2em", margin:'auto'}}>
-             <Select 
-             options={arrayOfLocation} 
-             value={place} 
-             defaultValue={{ label: "Select a city", value: "" }}
-             onChange={(value)=>{console.log(value)
-             setPlace(value)}}/>
-        </form>
-      
+        <form style={{width:"15em", marginLeft:"24em", marginBottom:"2em", margin:'auto', paddingBottom:"2em"}}>
+          <select className="custom-select"
+           onChange={(event)=>setPlace(event.target.value)}>
 
+            <option value="defaultValue">choose a location</option>
+            {roomPages.map((room) => {
+                    return <option key={room.id} value={room.location} >
+                            {room.location}
+            </option>
+          })}
+          </select>
+        </form>
       {roomsToRender()}
     </div>
   );
