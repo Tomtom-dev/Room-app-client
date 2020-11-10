01445dd381929b21f037319da9a7a5f3a8ed1afb
Tomtom-dev <thomasheddadji0@gmail.com>
2020-11-10 11:48:26 +0100

    create animation flip for roompage

--- src/component/RoomPageCard.js
+++ src/component/RoomPageCard.js
@@ -13,17 +13,24 @@ export default function RoomPageCard(props) {
     return (
         <div>
             <section className="RoomCard" >
-                <img src={image} className="" alt={"room "}/>
-                <div className="info-room">
-                    <h5>{title}</h5>
-                    <p>{description}</p>
-                    <p>City : {location}</p>
-                    {age?(<Link to={`rooms/${id}`}>
-                        <button>See more</button>
-                    </Link>) : <Link to={`login/`}>
-                    <p>Log in to see more</p>
-                    </Link>}
+                <div className="RoomCard-inner">
+                    <div className="RoomCard-front">
+                        <img src={image} className="" alt={"room "}/>
+                    </div>
+                    <div className="RoomCard-back">
+                            <div className="info-room">
+                                <h5>{title}</h5>
+                                <p>City : {location}</p>
+                                {age?(<Link to={`rooms/${id}`}>
+                                    <button>See more</button>
+                                </Link>) : <Link to={`login/`}>
+                                <p>Log in to see more</p>
+                                </Link>}
+                            </div>
+                    </div>
                 </div>
+                
+               
             </section>
         </div>
         
